130276f898289aa371cdf7aa9d277510724dc5b5
vinayaka-pu <102621844+vinayaka-pu@users.noreply.github.com>
2022-04-09 21:27:06 +0530

    Update SearchBox.jsx

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -2,40 +2,52 @@ import React from "react";
 import { useState } from "react";
 import './SearchBox.css';
 
-
 export const SearchBox = () => {
-    // const [searchResults, setSearchResults] = useState([]);
-    const [searchAddress, setSearchAddress] = useState("");
+    const [searchAddress, setSearchAddress] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
+    const [searchThis, setSearchThis] = useState('');
     let count = 1;
 
     const handleChange = (e) => {
-        const searchTerm = e.target.value;
-        setSearchTerm(searchTerm);    
+        const searchItem = e.target.value;
+        setSearchTerm(searchItem);    
     }
 
+
     const handleClick = (e) => {
-        const url = `https://72ih8opnm2.execute-api.ap-south-1.amazonaws.com/live/`;
+        e.preventDefault();
+        const url = `https://kcp1t1hcj5.execute-api.ap-south-1.amazonaws.com/live2/bucket?company_name=${searchTerm}`;
         fetch(url)
         .then(res => res.json())
         .then(data => {
-            for(var i=0; i<data.length; i++){
-                if(data[i].company_name === searchTerm){
-                    // setSearchResults(data[i].company_name);
-                    setSearchAddress(data[i].location);
+                console.log(data)
+            // for(var i=0; i<data.length; i++){
+            //     if(data[i].company_name === searchTerm){
+                    setSearchAddress(data[0].location);
                     return;
-                }
-            }
-            // setSearchResults('');
-            setSearchAddress('Please enter a valid company name & also check uppercase-lowercase');
+            //     }
+            // }
+            // setSearchAddress(data[searchTerm].location);
+            
+            // setSearchAddress('Please enter a valid company name & also check uppercase-lowercase');
+            setSearchAddress("Company doesn't exist");
         })
         .catch(err => console.log(err));
         }
+
+        const handleKeyPress = (e) => {
+            if(e.key === 'Enter'){
+                setSearchThis(searchTerm)
+                handleClick();
+            }
+        }
+
+
     return (
         <div className='search-box'>
             <p id="mainTxt">Search the directory</p>
             <div className='search-box-input'>
-                <input type='text' onChange={handleChange} placeholder='Search for companies' id="box"/>
+                <input type='text' onKeyPress={handleKeyPress} onChange={handleChange} placeholder='Search for companies' id="box"/>
                 <svg xmlns="http://www.w3.org/2000/svg" height="36" viewBox="0 0 24 24" width="28" onClick={handleClick} id='icon'><path d="M0 0h24v24H0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
             </div>
             <br/>
